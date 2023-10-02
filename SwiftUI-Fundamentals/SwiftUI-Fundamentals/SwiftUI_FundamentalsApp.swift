f4820b75b145901d3a4fb9a78ee01fcae5884d92
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-02 14:51:35 +0200

    add starter project

---
+++ SwiftUI-Fundamentals/SwiftUI-Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI-Fundamentals
+//
+//  Created by Jan Armbrust on 02.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
